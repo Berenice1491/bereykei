30359751274fd90d567e546d587c4d35b1c2f76d
Berenice1491 <alganarasberenice@gmail.com>
2026-04-28 21:24:52 -0300

    vuelve a repetir si esta mal

--- main.js
+++ main.js
@@ -7,39 +7,49 @@
 
 //const nombreDeUsuario = prompt("Ingrese su nombre.");
 //alert("Hola, " + nombreDeUsuario);
-const dni = prompt("Ingrese su DNI");
-if (dni.length < 7 || dni.length > 9) {
+
+let dni = prompt("Ingrese su DNI");
+
+while (dni.length < 7 || dni.length > 9) {
   alert("DNI inválido");
-} else {
-  alert("DNI válido");
+  dni = prompt("Ingrese su DNI");
 }
 
 let nombre = prompt("Ingrese su nombre");
-if (nombre.length < 3 || nombre.length > 35 || nombre == "") {
+while (
+  nombre.length < 3 ||
+  nombre.length > 35 ||
+  nombre == "" ||
+  !/^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]+$/.test(nombre)
+) {
   alert("Nombre inválido");
-} else if (!/^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]+$/.test(nombre)) {
-  alert("Nombre inválido: solo se permiten letras");
+  nombre = prompt("Ingrese su nombre");
 }
-
 let apellido = prompt("Ingrese su apellido");
-if (apellido.length < 3 || apellido.length > 20 || apellido == "") {
+while (
+  apellido.length < 3 ||
+  apellido.length > 20 ||
+  apellido == "" ||
+  !/^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]+$/.test(apellido)
+) {
   alert("Apellido inválido");
-} else if (!/^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]+$/.test(apellido)) {
-  alert("Apellido inválido: solo se permiten letras");
+  apellido = prompt("Ingrese su apellido");
 }
 
 let email = prompt("Ingrese su email");
-if (email == "") {
+while (email == "" || !email.includes("@") || !email.includes(".")) {
   alert("Email inválido");
-} else if (!email.includes("@") || !email.includes(".")) {
-  alert("Email inválido");
+  email = prompt("Ingrese su email");
 }
 
 let edad = prompt("Ingrese su edad");
-if (isNaN(edad) || !/^\d+$/.test(edad)) {
-  alert("La edad debe contener solo números");
-} else if (edad > 17 && edad <= 101) {
-  alert("Edad válida");
-} else if (edad <= 17 || edad > 101) {
+while (
+  isNaN(edad) ||
+  !/^\d+$/.test(edad) ||
+  edad == "" ||
+  edad <= 17 ||
+  edad > 101
+) {
   alert("Edad inválida");
+  edad = prompt("Ingrese su edad");
 }
